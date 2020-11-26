ed0e395e382a113bffa9a3fe1eac009ed91ff762
GWeber1 <gabrielweber2001@gmail.com>
2020-11-25 23:15:28 -0300

    Update Tetris_python.py

--- Tetris_python/Tetris_python.py
+++ Tetris_python/Tetris_python.py
@@ -6,13 +6,13 @@ pygame.font.init()
 tela_tamanho = 800
 tela_largura = 700
 play_width = 300 
-play_height = 600 
-tamanho_bloco = 30
+play_height = 600  
+tamanho_bloco = 30 #tamanho das formas
 
 top_left_x = (tela_tamanho - play_width) // 2
 top_left_y = tela_largura - play_height
 
-#CORES
+#CORES = todas as cores usadas no jogo
 
 cor_verde = (0,255,0)
 cor_vermelho = (255,0,0)
@@ -25,7 +25,7 @@ cor_cinza = (128,128,128)
 cor_preta = (0,0,0)
 cor_branca = (255, 255, 255)
 
-#FORMATOS
+#FORMATOS = matrizes de todas as peças e suas possíveis posições
 
 S = [['.....',
       '.....',
@@ -133,25 +133,25 @@ formas = [S, Z, I, O, J, L, T] #Lista de formas possíveis
 cores_formas = [(cor_verde),(cor_vermelho),(cor_azul_claro), (cor_amarelo),(cor_laranja), (cor_azul),(cor_roxo)] # índice de cores para as formas
 # indíce de representação das formas
 
-class Peca(object):
+class Peca(object): #superclasse
     def __init__(self, x, y, forma):
         self.x = x
         self.y = y 
         self.forma = forma 
-        self.cor = cores_formas[formas.index(forma)]
-        self.rotacao = 0
+        self.cor = cores_formas[formas.index(forma)] #associa formas com cores
+        self.rotacao = 0 # variável para rotacionar peças
 
 def criar_area(posicao={}):
-    area = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+    area = [[(cor_preta) for _ in range(10)] for _ in range(20)] #Criação da área de jogo
 
     for i in range(len(area)):
         for j in range(len(area[i])):
             if (j,i) in posicao:
-                c = posicao[(j,i)]
+                c = posicao[(j,i)] 
                 area[i][j] = c
     return area 
 
-def converte_forma(forma):
+def converte_forma(forma): 
     posicoes = []
     format = forma.forma[forma.rotacao % len(forma.forma)]
 
@@ -166,8 +166,8 @@ def converte_forma(forma):
     
     return posicoes
 
-def espaco_valido(forma, area):
-    posicao_aceita = [[(j, i) for j in range(10) if area[i][j] == (0,0,0)] for i in range(20)]
+def espaco_valido(forma, area): #função de delimitação do espaço válido
+    posicao_aceita = [[(j, i) for j in range(10) if area[i][j] == (cor_preta)] for i in range(20)]
     posicao_aceita = [j for sub in posicao_aceita for j in sub]
 
     formato = converte_forma(forma)
@@ -178,7 +178,7 @@ def espaco_valido(forma, area):
                 return False 
     return True
 
-def checa_perda(posicoes):
+def checa_perda(posicoes): # função que verifica se o jogador perdeu
     for pos in posicoes:
         x, y = pos 
         if y < 1:
@@ -186,16 +186,16 @@ def checa_perda(posicoes):
 
     return False
 
-def pega_forma():
+def pega_forma(): #função que aleatoriza as formas que descem
     return Peca(5, 0, random.choice(formas))
 
-def desenha_texto(superficie, texto, tamanho, cor):
+def desenha_texto(superficie, texto, tamanho, cor): #função que escreve na tela
     fonte = pygame.font.SysFont("comicsans", tamanho, bold=True)
     label = fonte.render(texto, 1, cor)
 
     superficie.blit(label, (top_left_x + play_width/2 - (label.get_width()/2), top_left_y + play_height/2 - label.get_height()/2))
 
-def desenha_area(superficie, area):
+def desenha_area(superficie, area): #função que deixa a área quadriculada
     sx = top_left_x
     sy = top_left_y
 
@@ -204,11 +204,11 @@ def desenha_area(superficie, area):
         for j in range(len(area[i])):
             pygame.draw.line(superficie, (cor_cinza), (sx+j*tamanho_bloco, sy),(sx + j*tamanho_bloco, sy + play_height))
 
-def limpa_linhas(area, sorteado):
+def limpa_linhas(area, sorteado): #função que limpa a linha cada vez que uma for preenchida
     inc = 0
     for i in range(len(area)-1,-1,-1):
         linha = area[i]
-        if (0,0,0) not in linha: 
+        if (cor_preta) not in linha: 
             inc += 1
             ind = i 
             for j in range(len(linha)):
@@ -225,7 +225,7 @@ def limpa_linhas(area, sorteado):
                 sorteado[new_key] = sorteado.pop(key)
     return inc
 
-def desenha_proxima_forma(forma, superficie):
+def desenha_proxima_forma(forma, superficie): #função que escreve e desenha na tela as próximas peças a virem
     font = pygame.font.SysFont('comicsans', 30)
     label = font.render('Próxima Peça', 1, (cor_branca))
 
@@ -241,7 +241,7 @@ def desenha_proxima_forma(forma, superficie):
     
     superficie.blit(label, (sx+10, sy-30))
 
-def atualiza_score(nscore):
+def atualiza_score(nscore): #função que envia os pontos para o .txt
     score = score_maximo()
 
     with open('pontuacao.txt','w') as f:
@@ -250,14 +250,14 @@ def atualiza_score(nscore):
         else:
             f.write(str(nscore))
 
-def score_maximo():
+def score_maximo(): #função que verifica qual a maior pontuação já feita no jogo
     with open('pontuacao.txt', 'r') as f:
         linhas = f.readlines()
         score = linhas[0].strip()
     
     return score
 
-def desenha_janela(superficie, area, score = 0, ultimo_score = 0):
+def desenha_janela(superficie, area, score = 0, ultimo_score = 0): #Função que coloca os dados na tela de jogo
     superficie.fill((cor_preta))
 
     pygame.font.init()
@@ -289,9 +289,9 @@ def desenha_janela(superficie, area, score = 0, ultimo_score = 0):
 
     desenha_area(superficie, area)
 
-def main(inicia): 
+def main(inicia): #Função que será chamado ao iniciar o jogo
     pygame.init()
-    pygame.mixer.music.load('buttercup.mp3')
+    pygame.mixer.music.load('buttercup.mp3') #trilha sonora
     pygame.mixer.music.play()
     ultimo_score = score_maximo()
     posicoes = {}
@@ -301,13 +301,13 @@ def main(inicia):
     run = True 
     peca_atual = pega_forma()
     proxima_peca = pega_forma()
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock() #define o tempo de atualização da tela
     tempo_queda = 0
-    velocidade_queda = 0.27 
+    velocidade_queda = 0.27 #velocidade em que a peça cai
     tempo_nivel = 0
-    score = 0
+    score = 0 #pontos começam em 0
     
-    while run:
+    while run: #loop do jogo
         area = criar_area(posicoes)
         tempo_queda += clock.get_rawtime()
         tempo_nivel += clock.get_rawtime()
@@ -330,56 +330,54 @@ def main(inicia):
                 run = False 
                 pygame.display.quit()
         
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+            if event.type == pygame.KEYDOWN: #enquanto a peça desce
+                if event.key == pygame.K_LEFT: #se a tecla pressionada for <-
                     peca_atual.x -= 1
-                    if not(espaco_valido(peca_atual, area)):
-                        peca_atual.x += 1
-                if event.key == pygame.K_RIGHT:
+                    if not(espaco_valido(peca_atual, area)): 
+                        peca_atual.x += 1 
+                if event.key == pygame.K_RIGHT: #se a tecla pressionada for ->
                     peca_atual.x += 1
                     if not(espaco_valido(peca_atual, area)):
                         peca_atual.x -= 1
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN: # se a tecla pressionada for ^
                     peca_atual.y += 1
                     if not(espaco_valido(peca_atual, area)):
                         peca_atual.y -= 1
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP: #se a tecla pressionada for ^
                     peca_atual.rotacao += 1
                     if not(espaco_valido(peca_atual, area)):
                         peca_atual.rotacao -= 1
-            if pygame.mixer.get_busy() == True:
-                pygame.mixer.music.play()
         
-        forma_pos = converte_forma(peca_atual)
+        forma_pos = converte_forma(peca_atual) #lança mais uma peça após a peça atual cair
 
         for i in range(len(forma_pos)):
             x, y = forma_pos[i]
             if y > -1:
-                area[y][x] = peca_atual.cor
+                area[y][x] = peca_atual.cor #joga a peça na área de jogo
         
         if mudar_peca:
             for pos in forma_pos: 
                 p = (pos[0], pos[1])
-                posicoes[p] = peca_atual.cor
+                posicoes[p] = peca_atual.cor #armazena a peça na lista
             peca_atual = proxima_peca 
             proxima_peca = pega_forma()
             mudar_peca = False 
-            score += limpa_linhas(area, posicoes) * 10
+            score += limpa_linhas(area, posicoes) * 10 #adiciona 10 pontos após completar uma linha
         
         desenha_janela(inicia, area, score, ultimo_score) 
         desenha_proxima_forma(proxima_peca, inicia)
-        pygame.display.update()
+        pygame.display.update() #atualiza as informações na tela
 
-        if checa_perda(posicoes):
+        if checa_perda(posicoes): #termina o jogo se perdeu
             desenha_texto(inicia, 'VOCÊ PERDEU!', 80, (cor_branca))
             pygame.display.update()
             pygame.time.delay(1500)
             run = False 
             atualiza_score(score)
 
-def menu_principal(inicia):
+def menu_principal(inicia): #primeira tela ao iniciar o jogo
     run = True 
-    while run:
+    while run: #loop de permanência da tela
         inicia.fill((cor_preta))
         desenha_texto(inicia, 'Pressione qualquer tecla', 60, (cor_branca))
         pygame.display.update()
@@ -389,7 +387,7 @@ def menu_principal(inicia):
             if event.type == pygame.KEYDOWN:
                 main(inicia)
 
-    pygame.display.quit()
-inicia = pygame.display.set_mode((tela_tamanho, tela_largura))
-pygame.display.set_caption('Tetris') 
-menu_principal(inicia)       
+    pygame.display.quit() #sai do jogo ao ter a janela fechada pelo jogador
+inicia = pygame.display.set_mode((tela_tamanho, tela_largura)) #inicia o jogo com os parâmetros do tamanho da tela
+pygame.display.set_caption('Tetris') #define o nome no topo da janela
+menu_principal(inicia) #início da execução  
